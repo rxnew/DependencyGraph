7d50c401d05d1b72b3dea51c885524a7acdff6a5
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-04-18 19:26:06 +0900

    Revert "Revert "init""
    This reverts commit db3fb3e338414c4ecb2b7549c0a226f03b84bd8e.

---
+++ constructor.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file constructor.hpp
+ * @brief header of Constructor class
+ */
+
+#pragma once
+
+#include "dag/graph.hpp"
+
+namespace dep {
+using dag::Graph;
+
+template <class Node>
+class Constructor {
+ private:
+  Graph<V> graph_;
+  Vertices sources_;
+  Vertices footprints_;
+
+  auto leaveFootprint(const V& v) const -> void;
+  auto existFootprint(const V& v) const -> bool;
+
+ public:
+  Constructor() = default;
+  explicit Constructor(Graph&& graph);
+  explicit Constructor(size_t size);
+  template <class OrderedVertices>
+  explicit Constructor(const Vertices& nodes);
+  Constructor(const Constructor&) = default;
+  Constructor(Constructor&&) = default;
+  ~Constructor() = default;
+
+  auto getGraph() const -> const Graph<V>&;
+  auto addVertex(const V& v, const V& prev) -> bool;
+  auto addVertex(const V& v) -> void;
+  template <class OrderedVertices>
+  auto addVertices(const OrderedVertices& vertices) -> void;
+};
+}
+
+#include "impl/constructor_impl.hpp"
