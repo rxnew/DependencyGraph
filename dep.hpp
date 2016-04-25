d7c3846f1ea343f723bbb146747098460e00004c
Rxnew Axdseuan <rxnew.axdseuan@gmail.com>
2016-04-25 17:50:27 +0900

    Graphのエイリアス追加

--- dep.hpp
+++ dep.hpp
@@ -4,6 +4,9 @@
 #include "manager.hpp"
 
 namespace dep {
+template <class V>
+using Graph = graph::DirectedGraph<V>;
+
 // Please specialize the following function
 // This function returns 'true', if 'u' depends on 'v'.
 template <class V>
